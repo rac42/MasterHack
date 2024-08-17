2ea86f5f9ebb9737bdd035beb09f173ddfb409bf
RushikeshGaikwad78 <gaikwadrushi7898@gmail.com>
2024-08-17 19:58:24 +0530

    signin functionality added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,100 +1,110 @@
-import React, { useState } from 'react';
-import bg from "../assets/NGO.jpg";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+// import React from 'react'
 
-const Login = () => {
-    const [formData, setFormData] = useState({
-        email: "", password: ""
-    });
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import signupimg from "../assets/signupimg.png";
 
-    const [showPassword, setShowPassword] = useState(false);
+export default function Login() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    function changeHandler(event) {
-        setFormData((prevData) => {
-            return {
-                ...prevData,
-                [event.target.name]: event.target.value,
-            };
-        });
-        
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMsg("All fields are required!!!");
     }
-
-    function submitHandler(event) {
-      event.preventDefault();
-      console.log("Printing the form data");
-      console.log(formData);
+    try {
+      setLoading(true);
+      setErrorMsg(null);
+      const res = await fetch("/server/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        // console.log(data)
+        return setErrorMsg(data.message);
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/')
+      }
+    } catch (error) {
+      // console.log(error)
+      setLoading(false);
+      setErrorMsg(error.message);
     }
+  };
 
-    return (
-        <div className="flex min-h-screen">
-            
-            <div className="w-7/12 bg-cover bg-center" style={{ backgroundImage: `url(${bg})` }}></div>
-
-            
-            <div className="w-5/12 flex flex-col justify-center p-20">
-                <form className="space-y-6" onSubmit={submitHandler}>
-                   
-                    <div>
-                        <label className="block text-gray-700 text-sm font-medium mb-1">
-                            Email Address<sup className="text-red-500">*</sup>
-                        
-                        <input
-                            required
-                            type="email"
-                            value={formData.email}
-                            name="email"
-                            onChange={changeHandler}
-                            placeholder="Enter your email"
-                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                        </label>
-                    </div>
-
-                    
-                    <div>
-                        <label className="block text-gray-700 text-sm font-medium mb-1">
-                            Password<sup className="text-red-500">*</sup>
-                        
-                        <div className="relative">
-                            <input
-                                required
-                                type={showPassword ? "text" : "password"}
-                                value={formData.password}
-                                name="password"
-                                onChange={changeHandler}
-                                placeholder="Enter your password"
-                                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            />
-                            <span
-                                onClick={() => setShowPassword(!showPassword)}
-                                className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
-                            >
-                                {showPassword ? <AiOutlineEyeInvisible size={20} /> : <AiOutlineEye size={20} />}
-                            </span>
-                        </div>
-                        </label>
-                    </div>
-
-                    
-                    <button className="w-full bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition duration-300">
-                        Log In
-                    </button>
-                </form>
-
-                
-                <div className="flex items-center my-6">
-                    <div className="flex-grow h-px bg-gray-300"></div>
-                    <span className="mx-4 text-gray-500">or</span>
-                    <div className="flex-grow h-px bg-gray-300"></div>
-                </div>
-
-                
-                <button className="w-full bg-red-500 text-white py-3 rounded-lg hover:bg-red-600 transition duration-300 flex items-center justify-center">
-                    Sign in with Google
-                </button>
-            </div>
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-2 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left side */}
+        <div className="flex-1">
+          <img
+            class="h-auto rounded-lg "
+            src={signupimg}
+            alt="image description"
+          />
         </div>
-    );
-};
-
-export default Login;
+        {/* right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div className="">
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="">
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="*********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToBlue"
+              type="submit"
+              outline
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size='sm'/>
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Login"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Dont have an Account?</span>
+            <Link to="/SignUp" className="text-blue-500">
+              Sign-Up
+            </Link>
+          </div>
+          {errorMsg && (
+            <Alert className="mt-5" color="failure">
+              {errorMsg}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
--- server/Controllers/auth.controller.js
+++ server/Controllers/auth.controller.js
@@ -1,35 +1,72 @@
 import User from "../Models/user.model.js";
 import { errorHandler } from "../Utils/Error.js";
-import dotenv from 'dotenv'
-import bcryptjs from 'bcryptjs'
+import dotenv from "dotenv";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
-export const signup = async (req,res,next) => {
-    const {Name, email, password} = req.body;
+export const signup = async (req, res, next) => {
+  const { Name, email, password } = req.body;
 
-    console.log("Received Name:", Name);
-    console.log("Received Email:", email);
-    console.log("Received Password:", password);
+  if (!Name || !email || !password) {
+    next(errorHandler(400, "All fields are required"));
+  }
 
-    if(!Name || !email || !password){
-        next(errorHandler(400,"All fields are required"))
+  // const salt_rounds = process.env.SALT_ROUND;
+  // console.log(salt_rounds)
+
+  // const hashedPassword = bcryptjs.hashSync(password,10);
+  // console.log(1);
+  const newUser = new User({ Name, email, password });
+  // console.log(2);
+  try {
+    // console.log(3);
+    await newUser.save();
+    res.json("sign up successfull!!!");
+  } catch (err) {
+    // console.log(4);
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+//   console.log(1)
+  const { email, password } = req.body;
+//   console.log(req.body)
+
+  if (!email || !password || email === "" || password === "") {
+    // console.log(2)
+    return next(errorHandler(400, "All fields are requiered !!!"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(400, "Not a valid user!!!"));
     }
 
-    // const salt_rounds = process.env.SALT_ROUND;
-    // console.log(salt_rounds)
+    const validPass = (password===validUser.password);
 
-    // const hashedPassword = bcryptjs.hashSync(password,10);
-    // console.log(1);
-    const newUser = new User ({Name,email,password});
-    // console.log(2);
-    try{
-        // console.log(3);
-        await newUser.save();
-        res.json("sign up successfull!!!");
-    }catch (err){
-        // console.log(4);
-        next(err);
+    if (!validPass) {
+      return next(errorHandler(400, "Not a valid user!!!"));
     }
 
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      "team16"
+    );
+
+    const {password: pass, ...rest} = validUser._doc;
+
+    res.status(200)
+        .cookie('access_token',token,{httpOnly:true})
+        .json(rest);
+
+  } catch (error) {
+    next(error);
+  }
 };
